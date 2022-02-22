7613c7970e78a3f75d0242cf18e4c366c279888b
tsmolarczyk <smolarczyk.thomas@gmail.com>
2022-02-22 20:23:52 +0100

    base

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
   //'http://localhost:4000/offers?page=2&limit=10&orderBy=id&direction=desc'
 console.log('lets go');
 
+const jobOffers = document.querySelector(".job-offers");
+
 let state = {
     jobs: [],
     page: 1,
 }
 
+function fetchOffers() {
+    getJobs('http://localhost:4000/offers');
+}
 
-function getJobs() {
-  fetch('http://localhost:4000/offers').then(
+
+function getJobs(link) {
+  fetch(link).then(
       (res) => res.json())
       .then((data) => {
-              let jobs = data.results;
-              state.jobs = jobs;
-              console.log(data);
+          console.log(data.data.records);
+        let jobs = data.data.records;
+          state.jobs = jobs;
+          console.log(jobs)
+          render();
         });
     }
 
-
-getJobs();
-
-
-render(){
+function render() {
     state.jobs.forEach((job) => {
 
         //empty box
         const jobElement = document.createElement("div");
-        const companyName = document.createElement("h1");        
+        const companyName = document.createElement("h1");
+        const jobDescription = document.createElement("p");
 
         //taking value
-        
+        companyName.textContent = job.company_name;
+        jobDescription.textContent = job.description;
 
         //adding classes
+        jobElement.classList.add("job-element");
+        companyName.classList.add("company-name");
+        jobDescription.classList.add("job-desc")
 
         //adding elements to DOM
-
+        jobOffers.appendChild(jobElement);
+        jobElement.appendChild(companyName);
+        jobElement.appendChild(jobDescription);
 
     })
 }
+
+fetchOffers();
